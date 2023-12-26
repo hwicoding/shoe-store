c65c0e7c64418d540f21e6e3c9035eaca0b89ce3
hwicoding <dlgnl117@gmail.com>
2023-12-26 17:22:55 +0900

    23.12.26 메인 생성, 유틸 생성

---
+++ shoes_store/src/com/javalec/util/ShareVar.java
@@ -0,0 +1,8 @@
+package com.javalec.util;
+
+public class ShareVar {
+	public static String dbName = "jdbc:mysql:127.0.0.1:3306;/useraddress?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	   public static String dbUser = "root";
+	   public static String dbPass = "qwer1234";
+	   public static int filename = 0;
+}
