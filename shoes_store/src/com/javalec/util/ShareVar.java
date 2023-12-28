14f6d1c5f8c4478768b7365f55f4d7b28d24a32a
shin-na-ra <nas221@naver.com>
2023-12-28 09:39:07 +0900

    2023.12.28 snr : ShareVar 총합 v0.2

--- shoes_store/src/com/javalec/util/ShareVar.java
+++ shoes_store/src/com/javalec/util/ShareVar.java
@@ -4,5 +4,10 @@ public class ShareVar {
 	public static String dbName = "jdbc:mysql://127.0.0.1/shoes?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
 	   public static String dbUser = "root";
 	   public static String dbPass = "qwer1234";
-	   public static int filename = 0;
+	   public static String userid="";
+	   public static String password="";
+	   public static String name="";
+	   public static String phone="";
+	   public static int filename=0;
 }
+
